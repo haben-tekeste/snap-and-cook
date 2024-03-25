fab2066e9b04898933537c982fdaa0e1a59e74e1
haben-tekeste <haben.t00@gmail.com>
2024-03-25 17:28:02 +0400

    Update types

--- src/types/navigation.ts
+++ src/types/navigation.ts
@@ -3,6 +3,11 @@ import { StackNavigationProp } from "@react-navigation/stack";
 export type TStackNavigation = {
   Home: any;
   Splash: any;
+  Ingredients: any;
+};
+
+export type StackParamList = {
+  IngredientsScreen: { data: any };
 };
 
 export type TStackTypes = StackNavigationProp<TStackNavigation>;
