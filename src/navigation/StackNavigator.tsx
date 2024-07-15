b90970ceacdef2127ab9b014782402043060e4dc
haben-tekeste <haben.t00@gmail.com>
2024-07-15 13:26:35 +0400

    Update stack navigation

--- src/navigation/StackNavigator.tsx
+++ src/navigation/StackNavigator.tsx
@@ -1,17 +1,28 @@
 import { createStackNavigator } from "@react-navigation/stack";
-import { SplashScreen, IngredientsScreen, HomeScreen } from "../screens";
-import Header from "../Components/Header";
+import {
+  SplashScreen,
+  IngredientsScreen,
+  HomeScreen,
+  RecipeListScreen,
+  RecipeDetailsScreen,
+} from "../screens";
+import { Button } from "react-native";
+import Header from "@components/Header";
+import Toggle from "@components/ToggleBtn";
+import { useAppSelector } from "../store";
+
 const Stack = createStackNavigator();
 
 export const StackNavigator = () => {
+  const { theme } = useAppSelector((state) => state.theme);
   return (
     <Stack.Navigator>
-      {/* <Stack.Screen
+      <Stack.Screen
         name="Splash"
         component={SplashScreen}
         options={{ headerShown: false }}
-      /> */}
-      {/* <Stack.Screen
+      />
+      <Stack.Screen
         name="Home"
         component={HomeScreen}
         options={{
@@ -20,10 +31,12 @@ export const StackNavigator = () => {
           headerStyle: {
             elevation: 0,
             shadowOpacity: 0,
+            backgroundColor: theme.colors.mainBackgroundColor,
           },
           headerLeft: () => null,
+          headerRight: () => <Toggle />,
         }}
-      /> */}
+      />
       <Stack.Screen
         name="Ingredients"
         component={IngredientsScreen}
@@ -33,7 +46,41 @@ export const StackNavigator = () => {
           headerStyle: {
             elevation: 0,
             shadowOpacity: 0,
+            backgroundColor: theme.colors.mainBackgroundColor,
           },
+          headerRight: () => <Toggle />,
+          headerLeft: () => null,
+        }}
+      />
+      <Stack.Screen
+        name="RecipeList"
+        component={RecipeListScreen}
+        options={{
+          headerShown: true,
+          headerTitle: () => <Header title="Recipes" />,
+          headerStyle: {
+            elevation: 0,
+            shadowOpacity: 0,
+            backgroundColor: theme.colors.mainBackgroundColor,
+          },
+          headerLeft: () => null,
+          headerRight: () => <Toggle />,
+          gestureEnabled: true,
+        }}
+      />
+      <Stack.Screen
+        name="RecipeDetails"
+        component={RecipeDetailsScreen}
+        options={{
+          headerShown: false,
+          headerTitle: () => null,
+          headerStyle: {
+            elevation: 0,
+            shadowOpacity: 0,
+            backgroundColor: theme.colors.mainBackgroundColor,
+          },
+          headerLeft: () => null,
+          headerRight: () => <Toggle />,
         }}
       />
     </Stack.Navigator>
