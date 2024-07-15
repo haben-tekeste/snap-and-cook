5045962f038b002871a0f2b06bd30348484e50ba
haben-tekeste <haben.t00@gmail.com>
2024-07-15 13:29:40 +0400

    Add export for new screens

--- src/screens/index.tsx
+++ src/screens/index.tsx
@@ -1,3 +1,5 @@
 export { default as HomeScreen } from "./HomeScreen";
 export { default as SplashScreen } from "./SplashScreen";
 export { default as IngredientsScreen } from "./IngredientsScreen";
+export { default as RecipeListScreen } from "./RecipeListScreen";
+export { default as RecipeDetailsScreen } from "./RecipeDetailsScreen";
