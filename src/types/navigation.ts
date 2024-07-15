29a53261a90772e3c9be25582959e1da9293b2bc
haben-tekeste <haben.t00@gmail.com>
2024-07-15 13:25:57 +0400

    Update navigation types

--- src/types/navigation.ts
+++ src/types/navigation.ts
@@ -4,10 +4,14 @@ export type TStackNavigation = {
   Home: any;
   Splash: any;
   Ingredients: any;
+  RecipeList: any;
+  RecipeDetails: any;
 };
 
 export type StackParamList = {
   IngredientsScreen: { data: any };
+  RecipeListScreen: { data: string };
+  RecipeDetailsScreen: { data: number };
 };
 
 export type TStackTypes = StackNavigationProp<TStackNavigation>;
